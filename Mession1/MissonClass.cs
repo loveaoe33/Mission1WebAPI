8e62b136a1701f44801a579ee7a6587ae9f3dde8
loveaoe33 <loveaoe33@gmail.com>
2022-12-25 23:26:41 +0800

    20221225UpdateClass

--- Mession1/MissonClass.cs
+++ Mession1/MissonClass.cs
@@ -11,26 +11,10 @@ namespace Mession1
 
         public class loginClass
         {
-            private String account;
+            private String account{ get;set;}
             //回傳
-            private String playUrl;
-            public loginClass() { }
-            public void setaccount(String account)
-            {
-                this.account = account;
-            }
-            public void setplayUrl(String playUrl)
-            {
-                this.playUrl = playUrl;
-            }
-            public String getaccount()
-            {
-                return account;
-            }
-            public String getplayUrl()
-            {
-                return playUrl;
-            }
+            private String playUrl{ get;set;}
+
         }
 
 
@@ -38,125 +22,34 @@ namespace Mession1
         public class chePoinClass
         {
 
-            private String account;
-            private int userBalance;
+            private String account{ get;set;}
+            //回傳
+            private decimal userBalance{ get;set;}
 
-            public void setaccount(String account)
-            {
-                this.account = account;
-            }
-            public void setuserBalance(int userBalance)
-            {
-                this.userBalance = userBalance;
-            }
-
-            public String getaccount()
-            {
-                return account;
-            }
-
-            public int getuserBalance()
-            {
-                return userBalance;
-            }
+           
 
         }
 
 
         public class poinLogClass
         {
-            private String account;
-            private String page;
-            private String limit;
+            private String account{ get;set;}
+            private int page{ get;set;}
+            private int limit{ get;set;}
             ///回傳
-            private int total;
-            private String data;
+            private int total{ get;set;}
+            private String data{ get;set;}
 
 
-            public void setaccount(String account)
-            {
-                this.account = account;
-            }
-
-            public void setPage(String page)
-            {
-                this.page = page;
-            }
-
-            public void setlimit(String limit)
-            {
-                this.limit = limit;
-            }
-            public void settotal(int total)
-            {
-                this.total = total;
-            }
-            public void setdata(String data)
-            {
-                this.data = data;
-            }
-
-
-            public String getaccount()
-            {
-                return account;
-            }
-            public String getpage()
-            {
-                return page;
-
-            }
-            public String getlimit()
-            {
-                return limit;
-
-            }
-            public int gettotal()
-            {
-                return total;
-
-            }
-            public String getdata()
-            {
-                return data;
-
-            }
-            //
+          
         }
 
 
         public class transpClass
         {
-            private String account;
-            private int balance;
+            private String account{ get;set;}
+            private decimal balance{ get;set;}
             ///回傳資料
-            private int userBalance;
-            public void setaccount(String account)
-            {
-                this.account = account;
-            }
-
-            public void setbalance(int balance)
-            {
-                this.balance = balance;
-            }
-            public void setuserBalance(int userBalance)
-            {
-                this.userBalance = userBalance;
-            }
-
-            public String getaccount()
-            {
-                return account;
-            }
-            public int getbalance()
-            {
-                return balance;
-            }
-            public int getuserBalance()
-            {
-                return userBalance;
-            }
         }
     }
 }
